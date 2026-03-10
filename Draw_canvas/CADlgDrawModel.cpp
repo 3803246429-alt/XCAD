57e1893fc7892e479e13b10a16e12a65b3b28ca9
3803246429-alt <3803246429@qq.com>
2026-03-10 18:02:24 +0800

    Delete Draw_canvas directory

--- Draw_canvas/CADlgDrawModel.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "pch.h"
-#include "../CADDlg.h"
-
-//绘制当前模型中的全部图元
-void CCADDlg::DrawModel(CDC* pDC) {
-    if (!pDC) return;
-
-    m_shapeMgr.DrawAll(pDC, m_transform, m_bShowPoints);
-}
